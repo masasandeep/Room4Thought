0a9718df774a14ee0b5344f42a930c888278a9ab
masasandeep <masasandeep12@gmail.com>
2024-05-14 20:22:27 +0530

    deploy

--- studybud/settings.py
+++ studybud/settings.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+ALLOWED_HOSTS = ['studybud1.onrender.com']
